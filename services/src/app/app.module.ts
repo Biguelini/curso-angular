cac1b15ed56625f1666e1ee71f2986fec75dbc18
Biguelini <biguelinijoao@gmail.com>
2023-05-18 15:02:34 -0300

    42/163

--- services/src/app/app.module.ts
+++ services/src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
+import { CursosService } from './cursos/cursos.service';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CriarCursoModule,
+    CursosModule
   ],
-  providers: [],
+  //providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
